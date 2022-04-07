ee7476b86fd61257cd59466db3ddc79a5d704e38
Grzegorz Ko <giantcaterpillar@gmail.com>
2022-04-07 12:09:27 +0200

    refactor

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -15,9 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -43,6 +41,22 @@ public class TestBase {
         return elements.get(new Random().nextInt(elements.size()));
     }
 
+    private static Stream<Arguments> newUsersSet() {
+        return Stream.of(
+                Arguments.of("Ace Ventura", "pet@detective.com", "123456789"),
+                Arguments.of("Bob Ross", "fun@painting.com", "abcdefg"),
+                Arguments.of("Gary Moore", "sustain@infinity.com", "123123123")
+        );
+    }
+
+    private static Stream<Arguments> newUsersSet2() {
+        return Stream.of(
+                Arguments.of("Ace", "Ventura", "pet@detective.com", 39),
+                Arguments.of("Bob", "Ross", "fun@painting.com", 66),
+                Arguments.of("Gary", "Moore", "sustain@infinity.com", 29)
+        );
+    }
+
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver(options);
@@ -58,22 +72,6 @@ public class TestBase {
         return driver;
     }
 
-    private static Stream<Arguments> newUsersSet() {
-        return Stream.of(
-                Arguments.of("Ace Ventura", "pet@detective.com", "123456789"),
-                Arguments.of("Bob Ross", "fun@painting.com", "abcdefg"),
-                Arguments.of("Gary Moore", "sustain@infinity.com", "123123123")
-        );
-    }
-
-    private static Stream<Arguments> newUsersSet2() {
-        return Stream.of(
-                Arguments.of("Ace",  "Ventura", "pet@detective.com", 39),
-                Arguments.of("Bob",  "Ross", "fun@painting.com", 66),
-                Arguments.of("Gary", "Moore", "sustain@infinity.com", 29)
-        );
-    }
-
     public boolean isElementVisible(String element) {
         try {
             return driver.findElement(By.cssSelector(element)).isDisplayed();
@@ -81,5 +79,4 @@ public class TestBase {
             return false;
         }
     }
-
 }
